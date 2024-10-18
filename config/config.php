a6b1a26088d27a74a3149e186777e0c9ad3ef497
cmp <chichomathew@gmail.com>
2024-10-19 00:42:09 +0700

    +config folder

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+try{
+    //host
+    define("HOST", "localhost");
+
+    //dbname
+    define("DBNAME", "cofee-blend");
+
+    //user
+    define("USER", "ROOT");
+
+    //pass
+    define("PASS", "");
+
+
+    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."",USER, PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}  catch(PDOException $Exception){
+
+    echo $Exception->getMessage();
+}
