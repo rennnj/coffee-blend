fa3f71399b76a89b8d52507f4d2f571ac0499083
Samuel Yahya <samuelyahya25@gmail.com>
2024-11-12 10:42:42 +0700

    Done folder 8

---
+++ admin-panel/index.php
@@ -0,0 +1,82 @@
+<?php require "layouts/header.php" ?>
+
+<div class="row">
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Products</h5>
+        <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+        <p class="card-text">number of products: 8</p>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Orders</h5>
+
+        <p class="card-text">number of orders: 4</p>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Bookings</h5>
+
+        <p class="card-text">number of bookings: 4</p>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Admins</h5>
+
+        <p class="card-text">number of admins: 3</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require "layouts/footer.php" ?>
+
+<!--  <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table> -->
